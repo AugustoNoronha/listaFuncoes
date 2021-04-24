45edf84bdc4c38b5dd2f7b00e2edffcda2254dde
AugustoNoronha <64671692+AugustoNoronha@users.noreply.github.com>
2021-04-24 14:41:13 -0300

    priemiro commit

---
+++ main.swift
@@ -0,0 +1,60 @@
+// var divisao: Int = 0
+// func printMessage(numero: Int = 4) {
+//   print(numero);
+//   if(numero%2 == 0){
+//     divisao = numero/2 ;
+//     print(divisao);
+//   }
+// }
+// printMessage()
+
+// var positivo:Bool = true 
+// var negativo:Bool = false
+// func numeroPositivoNegativo(numeroPouN:Int = -1){
+//   if(numeroPouN > 0){
+//     print(positivo)
+//   }else{
+//     print(negativo)
+//   }
+// }
+
+// numeroPositivoNegativo()
+
+// func resultado(media:Double = 6.9){
+//   if(media <= 0.0 || media >= 4.9){
+//     print("D")
+//   }
+//   if(media <= 5.0 || media >= 6.9){
+//     print("C")
+//      return
+//   }
+//   if(media <= 7.0 || media >= 8.9){
+//      print("B")
+//     return
+//   }
+//   if(media <= 9.0 || media >= 10.0){
+//       print("A")
+//     return
+//   }
+// }
+
+// resultado()
+// var soma: Int = 0
+// func recebeArrey(lista:[Int] = [2, 3, 26, 37, 38, 48, 60, 62, 75, 78]){
+//  soma = lista.reduce(0, +)
+//  print(soma)
+// }
+
+// recebeArrey()
+
+// func dividir(number: [Int]){
+//   number.forEach {
+//     element In
+//     let div = element/2
+//     if(div >= 1){
+//       print(div)
+//     }
+//   }
+// }
+
+// dividir(number:[2,3,4,6])
